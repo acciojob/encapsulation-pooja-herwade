bef93098892ac5ea65bb2f457775d45f22a59405
pooja-herwade <156627026+pooja-herwade@users.noreply.github.com>
2024-11-13 21:58:38 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly ob=new RWOnly();
+    //ob.name="Pooja";
+  //'nm' has private access in 'RWOnly'
+    ob.setname("Pooja");
+    System.out.println(ob.getname());
 }

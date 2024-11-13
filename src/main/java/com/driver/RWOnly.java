a263b626192a039cf66a84f6a41aada11a66ff87
pooja-herwade <156627026+pooja-herwade@users.noreply.github.com>
2024-11-13 21:30:46 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setname(String nm){
+        this.name=nm;
+    }
+    public String getname(){
+        return name;
+    }
 }
